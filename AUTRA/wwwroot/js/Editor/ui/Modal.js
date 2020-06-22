27ba6c49c75ff6e3292cfdf8641b7cd9e3d2ad27
Marwa <marwa.msalama@gmail.com>
2020-06-22 05:04:49 +0200

    Two Startup Models & minor ui - minus form validation (pending)

--- AUTRA/wwwroot/js/Editor/ui/Modal.js
+++ AUTRA/wwwroot/js/Editor/ui/Modal.js
@@ -6,3 +6,72 @@ $("#manMode").click(function () { //Hide Auto mode data if Manual mode selected
 $("#autoMode").click(function () { //Show Auto mode data if Manual mode not selected
     $("#autoModeData").fadeIn();
 }); 
+
+
+$('#modalOpenProjectTitle').click(function () {
+    $("#modalNewProjectDialog1").hide();
+    $("#modalOpenProjectDialog").show();
+    $('.fontBlue').removeClass("fontBlue");
+    $('#modalOpenProjectTitle').addClass("fontBlue");
+
+});
+
+$('#modalNewProjectTitle').click(function () {
+    $("#modalOpenProjectDialog").hide();
+    $("#modalNewProjectDialog1").show();
+    $('.fontBlue').removeClass("fontBlue");
+    $('#modalNewProjectTitle').addClass("fontBlue");
+});
+
+$('#modalFlipForward').click(function () {
+    $("#modalPage1").hide();
+    $("#modalPage2").show();
+});
+
+$('#modalFlipBackward').click(function () {
+    $("#modalPage2").hide();
+    $("#modalPage1").show();
+});
+
+
+
+// Wait for the DOM to be ready
+$(function () {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form[name='registration']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            firstname: "required",
+            lastname: "required",
+            email: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 5
+            }
+        },
+        // Specify validation error messages
+        messages: {
+            firstname: "Please enter your firstname",
+            lastname: "Please enter your lastname",
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            email: "Please enter a valid email address"
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+});
